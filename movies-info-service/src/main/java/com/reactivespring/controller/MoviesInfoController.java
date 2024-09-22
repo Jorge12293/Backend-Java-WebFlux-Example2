2fe72b248111c718e7f87500e062cd39dddb0de8
Jorge <jorgerivera12293@gmail.com>
2024-09-22 09:30:09 -0500

    Implemet API basic

---
+++ movies-info-service/src/main/java/com/reactivespring/controller/MoviesInfoController.java
@@ -0,0 +1,57 @@
+package com.reactivespring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.service.MoviesInfoService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RestController
+@RequestMapping(value = "v1")
+public class MoviesInfoController {
+    
+    @Autowired
+    private MoviesInfoService moviesInfoService;
+
+    @GetMapping("movie-infos")
+    public Flux<MovieInfo> getAllMovieInfos() {
+        return moviesInfoService.getAllMovieInfos();
+    }
+    
+    @GetMapping("movie-infos/{id}")
+    public Mono<MovieInfo> getAllMovieInfoById(@PathVariable String id) {
+        return moviesInfoService.getMovieInfoById(id);
+    }
+
+    @PostMapping("movie-infos")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<MovieInfo> addMovieInfo(@RequestBody MovieInfo movieInfo) {
+        return moviesInfoService.adMovieInfo(movieInfo);
+    }
+    
+
+    @PutMapping("movie-infos/{id}")
+    public Mono<MovieInfo> updateMovieInfo(@RequestBody MovieInfo movieInfo,@PathVariable String id) {
+        return moviesInfoService.updateMovieInfo(movieInfo,id);
+    }
+    
+    @PutMapping("movie-infos/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> deleteMovieInfo(@PathVariable String id) {
+        return moviesInfoService.deleteMovieInfo(id);
+    }
+}
---
+++ movies-info-service/src/main/java/com/reactivespring/service/MoviesInfoService.java
@@ -0,0 +1,45 @@
+package com.reactivespring.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.repository.MovieInfoRepository;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class MoviesInfoService {
+
+    @Autowired
+    private MovieInfoRepository movieInfoRepository;
+
+    public Mono<MovieInfo> adMovieInfo(MovieInfo movieInfo) {
+        return movieInfoRepository.save(movieInfo);
+    }
+
+    public Flux<MovieInfo> getAllMovieInfos() {
+        return movieInfoRepository.findAll();
+    }
+
+    public Mono<MovieInfo> getMovieInfoById(String id) {
+        return movieInfoRepository.findById(id);
+    }
+
+    public Mono<MovieInfo> updateMovieInfo(MovieInfo updatedMovieInfo, String id) {
+        return movieInfoRepository.findById(id)
+            .flatMap(movieInfo->{
+                movieInfo.setCast(updatedMovieInfo.getCast());
+                movieInfo.setName(updatedMovieInfo.getName());
+                movieInfo.setRelease_date(updatedMovieInfo.getRelease_date());
+                movieInfo.setYear(updatedMovieInfo.getYear());
+                return movieInfoRepository.save(movieInfo);
+            });
+    }
+
+    public Mono<Void> deleteMovieInfo(String id) {
+        return movieInfoRepository.deleteById(id);
+    }
+
+}
