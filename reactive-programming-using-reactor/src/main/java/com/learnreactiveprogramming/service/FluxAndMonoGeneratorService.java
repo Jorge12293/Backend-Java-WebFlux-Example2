e6bda40efe0f85e38f83943528236de2abcb7108
Jorge <jorgerivera12293@gmail.com>
2024-09-22 05:32:31 -0500

    Implement Excercise Basic

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,9 +1,46 @@
 package com.learnreactiveprogramming.service;
 
+import java.util.List;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public class FluxAndMonoGeneratorService {
 
+    public Flux<String> namesFlux(){
+        return Flux.fromIterable(List.of("Alex","Ben","Chloe")).log();
+    }
+
+    public Mono<String> namesMono(){
+        return Mono.just("Alex").log();
+    }
+
+    public Flux<String> namesFluxMap(){
+        return Flux.fromIterable(List.of("Alex","Ben","Chloe"))
+            .map(String::toUpperCase)
+            .log();
+    }
+
     public static void main(String[] args) {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+    
         
+        fluxAndMonoGeneratorService.namesFlux()
+            .subscribe(name->{
+                System.out.println("Name is :"+name);
+            });
+
+        fluxAndMonoGeneratorService.namesMono()
+            .subscribe(name->{
+                System.out.println("Mono Name is :"+name);
+            });
+        
+
+        fluxAndMonoGeneratorService.namesFluxMap()
+            .subscribe(name->{
+                System.out.println("Name is :"+name);
+            });
+
     }
 
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -2,6 +2,28 @@ package com.learnreactiveprogramming.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FluxAndMonoGeneratorServiceTest {
+import org.junit.jupiter.api.Test;
 
+import reactor.test.StepVerifier;
+
+class FluxAndMonoGeneratorServiceTest {
+    
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorServiceTest = 
+        new FluxAndMonoGeneratorService();
+
+    @Test
+    void namesFlux() {
+        // given
+
+        // when
+        var namesFlux = fluxAndMonoGeneratorServiceTest.namesFlux();
+
+        // then
+        StepVerifier.create(namesFlux)
+            .expectNext("Alex","Ben","Chloe")
+            .verifyComplete();
+    }   
+
+    
+    
 }
