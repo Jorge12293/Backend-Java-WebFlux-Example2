8d27c25641d187b672e2a3136c98a51ac9e95a35
Jorge <jorgerivera12293@gmail.com>
2024-09-28 17:27:25 -0500

    Implement Global Error

---
+++ movies-review-service/src/main/java/com/reactivespring/router/ReviewRouter.java
@@ -0,0 +1,45 @@
+package com.reactivespring.router;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.reactivespring.handler.ReviewHandler;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+
+@Configuration
+public class ReviewRouter {
+    
+    @Bean
+    public RouterFunction<ServerResponse> reviewsRoute(ReviewHandler reviewHandler){
+
+        /*return route()
+            .GET("/v1/hello-world",(request->
+                ServerResponse.ok().bodyValue("Hello World")))
+            .POST("/v1/reviews",request-> reviewHandler.addReview(request))    
+            .GET("/v1/reviews",request->reviewHandler.getReviews(request))
+            .build(); */
+        
+        return route()
+            .nest(path("/v1/reviews"), builder -> { 
+                builder
+                    .POST("",request-> reviewHandler.addReview(request))    
+                    .GET("",request->reviewHandler.getReviews(request))
+                    .PUT("/{id}",request-> reviewHandler.updateReview(request))
+                    .DELETE("/{id}",request->reviewHandler.deleteReview(request));
+            })
+            .GET("/v1/hello-world",(request->ServerResponse.ok().bodyValue("Hello World")))
+            .build();
+
+    }
+
+}
