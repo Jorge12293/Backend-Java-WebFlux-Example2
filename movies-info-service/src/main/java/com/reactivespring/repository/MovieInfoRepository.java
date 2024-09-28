f69e30fc871588bf527833c91e081f7cf863a4bc
Jorge <jorgerivera12293@gmail.com>
2024-09-28 05:33:23 -0500

    Test

--- movies-info-service/src/main/java/com/reactivespring/repository/MovieInfoRepository.java
+++ movies-info-service/src/main/java/com/reactivespring/repository/MovieInfoRepository.java
@@ -1,9 +1,11 @@
 package com.reactivespring.repository;
 
+import reactor.core.publisher.Flux;
+import com.reactivespring.domain.MovieInfo;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-import com.reactivespring.domain.MovieInfo;
-
 public interface MovieInfoRepository extends ReactiveMongoRepository<MovieInfo,String>{
+
+    Flux<MovieInfo>findByYear(Integer year);
 
 }
